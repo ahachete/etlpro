3b62df01a13db705e2921afb18ebf22583bfebcd
Álvaro Hernández Tortosa <aht@8kdata.com>
2016-07-01 06:43:10 +0200

    Created new ETL method based on database JOIN
    etl_co.py is basically re-implementing the MERGE JOIN operation in the
    application. We rather prefer to rely here on the database's
    implementation, which results in a significantly simpler and less
    error-prone application but also higher performance ETL process.

---
+++ etl_join.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import psycopg2
+from pymongo import MongoClient
+import database
+
+client = MongoClient("localhost:27017")
+db = client.etlpro
+orders = db.orders
+
+cnx = psycopg2.connect(database.dsn())
+cursor = cnx.cursor()
+
+join_query = """
+SELECT		o.*, i.items, t.trackings
+FROM		orders AS o
+		INNER JOIN (
+			SELECT		order_id, array_agg(ARRAY[id, qty, description, price]::text[]) AS items
+			FROM		items
+			GROUP BY	order_id
+		) AS i
+		ON (i.order_id = o.id)
+		INNER JOIN (
+			SELECT		order_id, array_agg(ARRAY[status, tmstmp]::text[]) AS trackings
+			FROM		tracking
+			GROUP BY	order_id
+		) AS t
+		ON (t.order_id = o.id)
+"""
+cursor.execute(join_query)
+
+for (order_id, first_name, last_name, shipping_address, items, trackings) in cursor:
+    item_list = []
+    tracking_list = []
+    doc = { "order_id" : order_id,
+            "first_name" : first_name,
+            "last_name" : last_name,
+            "shipping_address" : shipping_address,
+            "items" : item_list,
+            "tracking" : tracking_list }
+
+    for (item) in items:
+	 item_list.append( { "item_id" : item[0],
+                            "qty" : item[1],
+                            "description" : item[2],
+                            "price" : item[3] } )
+
+    for (tracking) in trackings:
+        tracking_list.append( { "status" : tracking[0],
+                                "timestamp" : tracking[1] } )
+
+    orders.insert_one(doc)
+
+cursor.close()
+cnx.close()
+
+client.close()
